c4f6656c833cbef9dfda2e3efdcde02357770629
Willem-Jan <wjzijderveld@gmail.com>
2016-04-01 18:26:08 +0200

    package.sh 1.0

---
+++ package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR=$(cd $(dirname $0) && pwd)
+
+OUTPUT="$DIR/artifact/"
+TSC=`which tsc`
+
+
+if [[ !  -d $OUTPUT ]]; then
+    mkdir -p $OUTPUT
+fi
+
+$TSC --outDir $OUTPUT/app/
+cp $DIR/index.html $OUTPUT/
+cp -r $DIR/tpl $OUTPUT/
+cp -r $DIR/resources $OUTPUT/
+
+cp -r $DIR/node_modules $OUTPUT/
+cp $DIR/package.json $OUTPUT/
+
+cd $OUTPUT/
+npm prune --production
